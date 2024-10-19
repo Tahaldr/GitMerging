941af1d62d96a011994f152a709d7c44e03d335d
Tahaldr <tahanaitdo@gmail.com>
2024-10-19 13:18:58 +0200

    Fix errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,17 @@
 const Hero = () => {
   return (
-    <>
+    <div className="flex justify-between items-center p-5">
       <div>
+        {/* Left nav section made by BilalnessMaster */}
         <div className='left_navbar '>
-          <h1 className='text-4xl text-green-400 bg-gray-50 px-2 py-2 rounded'>
+          <h1 className='text-4xl text-black bg-gray-50 px-2 py-2 rounded'>
             <i className='ri-arrow-left-right-line'></i>
           </h1>
         </div>
       </div>
 
-      <div className='flex justify-center items-center gap-10'>
-        {/* Right nav section */}
+      <div className="flex justify-between items-center gap-10">
+        {/* Right nav section made by Tahaldr */}
 
         <div>
           <ul className='flex gap-10'>
@@ -23,7 +24,7 @@ const Hero = () => {
           <button className='bg-black text-white px-3 py-2'>Get in touch</button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
