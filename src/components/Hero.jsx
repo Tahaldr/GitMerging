45b2ab47e670d06e42822fec6a9956df1be96908
Tahaldr <tahanaitdo@gmail.com>
2024-10-19 13:28:38 +0200

    fix other errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,27 @@
 const Hero = () => {
   return (
-
     <>
-    
-      
-
       <div className='max-w-screen-xl mx-auto flex justify-between items-center gap-10 mt-5'>
-      <div className='flex justify-center items-center gap-10'>
-      <div className='left_navbar '>
-          <h1 className='text-4xl text-green-400 bg-gray-50 px-2 py-2 rounded'>
-            <i className='ri-arrow-left-right-line'></i>
-          </h1>
+        <div className='flex justify-center items-center gap-10'>
+          <div className='left_navbar '>
+            <h1 className='text-4xl text-green-400 bg-gray-50 px-2 py-2 rounded'>
+              <i className='ri-arrow-left-right-line'></i>
+            </h1>
+          </div>
         </div>
-      </div>
 
-        {/* Right nav section */}
-
-        <div className="flex items-center gap-5">
-          <ul className='flex  gap-10'>
+        <div className='flex items-center gap-5'>
+          <ul className='flex gap-10'>
             <li>About</li>
             <li>Projects</li>
             <li>Contact</li>
           </ul>
-        <div>
-          <button className='bg-black text-white px-3 py-2'>Get in touch</button>
+          <div>
+            <button className='bg-black text-white px-3 py-2'>Get in touch</button>
+          </div>
         </div>
-        </div>
-     
       </div>
-      </>
+    </>
   );
 };
 
