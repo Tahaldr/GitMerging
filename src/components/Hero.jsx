afde363c026a848c495de31ca322f40cf9bc0f47
bilalnessMaster <jijahgiga@gmail.com>
2024-10-19 13:21:32 +0100

    first navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,26 @@
 const Hero = () => {
   return (
     <>
-      <div>
-        <div className='left_navbar '>
-          <h1 className='text-4xl text-green-400 bg-gray-50 px-2 py-2 rounded'>
+    
+      
+
+      <div className='max-w-screen-xl mx-auto flex justify-between items-center gap-10 mt-5'>
+     
+      <div className='left_navbar '>
+          <h1 className='text-4xl text-green-400 bg-gray-50 px-2 py-2 rounded-xl'>
             <i className='ri-arrow-left-right-line'></i>
           </h1>
         </div>
-      </div>
 
-      <div className='flex justify-center items-center gap-10'>
-        {/* Right nav section */}
-
-        <div>
+        <div className="flex gap-5 items-center">
           <ul className='flex gap-10'>
             <li>About</li>
             <li>Projects</li>
             <li>Contact</li>
           </ul>
-        </div>
         <div>
           <button className='bg-black text-white px-3 py-2'>Get in touch</button>
+        </div>
         </div>
       </div>
     </>
